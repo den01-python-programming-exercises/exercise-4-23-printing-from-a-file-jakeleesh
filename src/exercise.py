81e9fc9ec1456add63b68679395a9b08fc7302e0
Jake Lee <jake.lee@fdmgroup.com>
2021-10-15 22:41:58 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    try:
+        f = open("src\data.txt", "r")
+        print(f.read())
+    except:
+        print("Error")
+    finally:
+        f.close()
 
 if __name__ == '__main__':
     main()
